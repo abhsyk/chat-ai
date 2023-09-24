08d61128b490387189173d14d95fbeca2f215b39
Sho Harada <tmchtmtr09876@gmail.com>
2023-09-24 17:40:48 +0900

    TextAnimation

--- src/components/chat/MessageItem/index.tsx
+++ src/components/chat/MessageItem/index.tsx
@@ -1,10 +1,12 @@
 import { FC } from 'react';
 import { FcMindMap } from 'react-icons/fc';
 import { GrUser } from 'react-icons/gr';
+import { TextAnimation } from '../../ui';
 
 type Props = {
   isAiMessage: boolean;
   content: string;
+  // toAnimateText: boolean;
 };
 
 const MessageItem: FC<Props> = ({ isAiMessage, content }) => {
@@ -16,7 +18,19 @@ const MessageItem: FC<Props> = ({ isAiMessage, content }) => {
       <div className="message_avator_container">
         {isAiMessage ? <FcMindMap /> : <GrUser />}
       </div>
-      <p className="message_text">{content}</p>
+      <p className="message_text">
+        {
+          // if it is a message from AI, make it animation
+          isAiMessage ? (
+            <TextAnimation
+              speed={20} // ms
+              text={content}
+            />
+          ) : (
+            content
+          )
+        }
+      </p>
     </div>
   );
 };
---
+++ src/components/ui/TextAnimation/index.tsx
@@ -0,0 +1,32 @@
+import { FC, useEffect, useRef, useState } from 'react';
+
+// MessageItem
+
+type Props = {
+  speed: number; // ms
+  text: string;
+};
+
+const TextAnimation: FC<Props> = ({ speed = 20, text }) => {
+  const [placeholder, setPlaceholder] = useState(text[0]); // each char of the text
+  const index = useRef(0); // text index
+
+  useEffect(() => {
+    const tick = () => {
+      index.current++;
+      setPlaceholder((prevChar) => prevChar + text[index.current]);
+    };
+
+    /* Interval function "tick" until the current index of
+     the text (placeholder) is equal to the length of the text (index) */
+    if (index.current < text.length - 1) {
+      const addChar = setInterval(tick, speed);
+
+      return () => clearInterval(addChar);
+    }
+  }, [placeholder, speed, text]);
+
+  return <span>{placeholder}</span>;
+};
+
+export default TextAnimation;
