5175acff9236cb8fbca44bf0dbf2489a6aec5199
Sho Harada <tmchtmtr09876@gmail.com>
2023-09-24 16:13:50 +0900

    NewMessageInput

--- src/components/chat/NewMessageInput/index.tsx
+++ src/components/chat/NewMessageInput/index.tsx
@@ -1,7 +1,21 @@
 import { FC } from 'react';
+import { BsSend } from 'react-icons/bs';
 
 const NewMessageInput: FC = () => {
-  return <div className="new_messsage_input_container">NewMessageInput</div>;
+  return (
+    <div className="new_message_input_container">
+      <input
+        className="new_message_input"
+        placeholder="Send a message..."
+        value=""
+        onChange={() => {}}
+        onKeyDown={() => {}}
+      />
+      <div className="new_message_icon_container" onChange={() => {}}>
+        <BsSend color="grey" />
+      </div>
+    </div>
+  );
 };
 
 export default NewMessageInput;
