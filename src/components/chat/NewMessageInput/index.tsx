1ce6fd736384a2b72e14a8d510cba753e68ef66c
Sho Harada <tmchtmtr09876@gmail.com>
2023-09-25 12:16:58 +0900

    handling new message

--- src/components/chat/NewMessageInput/index.tsx
+++ src/components/chat/NewMessageInput/index.tsx
@@ -1,17 +1,49 @@
-import { FC } from 'react';
+import { FC, KeyboardEvent, useState } from 'react';
 import { BsSend } from 'react-icons/bs';
+import { v4 as uuid } from 'uuid';
+import { Content } from '../../../interfaces';
 
 const NewMessageInput: FC = () => {
+  const [content, setContent] = useState<string>('');
+
+  const proceedMessage = () => {
+    // create message obj
+    const message: Content = {
+      id: uuid(),
+      content,
+      isAiMessage: false,
+    };
+    console.log(message);
+
+    // append the message to the local store (redux)
+
+    // send message to the server
+
+    // reset value of the input
+    setContent('');
+  };
+
+  const handleSendMessage = () => {
+    if (content.length > 0) proceedMessage();
+  };
+
+  // press the enter key
+  const handleEnterPressed = (e: KeyboardEvent) => {
+    if (e.code === 'Enter' && content.length > 0) {
+      proceedMessage();
+    }
+  };
+
   return (
     <div className="new_message_input_container">
       <input
         className="new_message_input"
         placeholder="Send a message..."
-        value=""
-        onChange={() => {}}
-        onKeyDown={() => {}}
+        value={content}
+        onChange={(e) => setContent(e.target.value)}
+        onKeyDown={handleEnterPressed}
       />
-      <div className="new_message_icon_container" onChange={() => {}}>
+      <div className="new_message_icon_container" onClick={handleSendMessage}>
         <BsSend color="grey" />
       </div>
     </div>
